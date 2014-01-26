3261716bba22e1641990d30c1e929d0fa560eab6
Nick Erlikh <n.erlikh@gmail.com>
2014-01-26 22:53:46 +0700

    cleanup

--- public/js/controllers/header.js
+++ public/js/controllers/header.js
@@ -2,14 +2,4 @@
 
 angular.module('mean.system').controller('HeaderController', ['$scope', 'Global', function ($scope, Global) {
     $scope.global = Global;
-
-    $scope.menu = [{
-        'title': 'Articles',
-        'link': 'articles'
-    }, {
-        'title': 'Create New Task',
-        'link': 'tasks/create'
-    }];
-
-    $scope.isCollapsed = false;
 }]);
